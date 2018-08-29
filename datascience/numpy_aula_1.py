84a9fe2d44038a9d97a940225e005461cbb3c344
Pedro Henrique Lage Duarte <duartep@google.com>
2018-08-29 12:54:15 -0300

    primeiro commit

---
+++ datascience/numpy_aula_1.py
@@ -0,0 +1,20 @@
+#CALCULO DA MEDIA DA MEDIANA OU DO DESVIO PADRAO DE UM ARRAY
+
+import numpy as np
+
+numbers = [1,2,3,4,5]
+
+print(numbers)
+
+
+media = numpy.mean(numbers)
+
+print(media)
+
+mediana = numpy.median(numbers)
+
+print(mediana)
+
+desvio_padrao = numpy.std(numbers)
+
+print(desvio_padrao)
