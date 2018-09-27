eed47f711ed8e86639565069da4f5c00b06a8342
Pedro Duarte <pedrohlduarte@gmail.com>
2018-09-27 20:38:10 -0300

    testando matheus

--- COLETANDO/matheus.py
+++ COLETANDO/matheus.py
@@ -3,63 +3,44 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 
-page_nexa = "https://finance.yahoo.com/quote/NEXA/history/"
 
-page_nyse = "https://finance.yahoo.com/quote/%5ENYA/history?ltr=1"
+def coletando(page_nexa):
 
-page_nexa = requests.get(page_nexa)
-page_nexa= str(page_nexa.content)
+    page_nexa = requests.get(page_nexa)
+    page_nexa= str(page_nexa.content)
 
-page_nyse = requests.get(page_nyse)
-page_nyse= str(page_nyse.content)
+    soup_nexa = BeautifulSoup(page_nexa,"html.parser")
 
-soup_nexa = BeautifulSoup(page_nexa,"html.parser")
-soup_nyse = BeautifulSoup(page_nyse,"html.parser")
+    all_tables_nexa = soup_nexa.find_all("table")
+    tabela_nexa = soup_nexa.find("table")
 
+    tbody_nexa = tabela_nexa.find("tbody")
 
-all_tables_nexa = soup_nexa.find_all("table")
-tabela_nexa = soup_nexa.find("table")
+    tabela_data_nexa = []
+    tabela_valor_nexa = []
+    tabela_retorno_nexa = []
 
-all_tables_nyse = soup_nyse.find_all("table")
-tabela_nyse = soup_nyse.find("table")
+    valor = 0
+    valor_ant = 0
+    retorno = 0
 
-tbody_nexa = tabela_nexa.find("tbody")
-tbody_nyse = tabela_nyse.find("tbody")
-
-tabela_data_nexa = []
-tabela_valor_nexa = []
-tabela_retorno_nexa = []
-tabela_data_nyse = []
-tabela_valor_nyse = []
-tabela_retorno_nyse = []
-valor = 0
-valor_ant = 0
-retorno = 0
-
-for row in tbody_nexa.find_all("tr"):
-    cells = row.find_all("td")
-    data = cells[0].find(text=True)
-    #valor_ant = valor
-    valor = cells[5].find(text=True)
-    #retorno = np.log(valor/valor_ant)
-    tabela_data_nexa.append(data)
-    tabela_valor_nexa.append(valor)
-    #tabela_retorno_nexa.append(retorno)
-
-for row in tbody_nyse.find_all("tr"):
-    cells = row.find_all("td")
-    data = cells[0].find(text=True)
-    #valor_ant = valor
-    valor = cells[5].find(text=True)
-    #retorno = np.log(valor / valor_ant)
-    tabela_data_nyse.append(data)
-    tabela_valor_nyse.append(valor)
-    #tabela_retorno_nyse.append(retorno)
+    for row in tbody_nexa.find_all("tr"):
+        cells = row.find_all("td")
+        data = cells[0].find(text=True)
+        #valor_ant = valor
+        valor = cells[5].find(text=True)
+        #retorno = np.log(valor/valor_ant)
+        tabela_data_nexa.append(data)
+        tabela_valor_nexa.append(valor)
+        #tabela_retorno_nexa.append(retorno)
+    df = pd.DataFrame()
+    df["Data Nexa"] = tabela_data_nexa
+    df["Preço Nexa"] = tabela_valor_nexa
+    print(df)
 
 
-df = pd.DataFrame()
-df["Data Nexa"] = tabela_data_nexa
-df["Preço Nexa"] = tabela_valor_nexa
-df["Data Nyse"] = tabela_data_nyse
-df["Preço Nyse"] = tabela_valor_nyse
-print(df)
+page = ["https://finance.yahoo.com/quote/NEXA/history/"]
+
+lista_name = ["PETR4.SA","NEXA",]
+
+coletando(page[0])
