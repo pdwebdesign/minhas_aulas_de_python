35a3385d464de1d90a55cc2ebc443c1cf4ca4bff
unknown <digoguimaraes@outlook.com>
2018-10-10 20:33:11 -0300

    estou dando aula de lista

--- PYTHON_1/capitulo5/for.py
+++ PYTHON_1/capitulo5/for.py
@@ -1,6 +1,9 @@
-
-lista = [100,15,200,150,23]
-print(lista)
-print(lista[0])
-lista[0] = lista[1]
-print(lista)
+site = "somos todos muito trabalhadores #amor #love , boa tarde"
+pos = site.find("#")
+if pos == -1:
+    print("nao tem #")
+esp = site.find(" ",pos)
+palavra = site[pos + 1:esp]
+print(palavra)
+site = site[esp:]
+print(site)
