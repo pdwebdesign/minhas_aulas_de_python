f86d1dc8f5227b69953c0d6a3493120efa3dd93e
Pedro Duarte <pedrohlduarte@gmail.com>
2018-10-16 19:18:07 -0300

    16_10 aula

--- COLETANDO/aula_16_10.py
+++ COLETANDO/aula_16_10.py
@@ -2,7 +2,6 @@ import requests
 import numpy as np
 import pandas
 from bs4 import BeautifulSoup
-
 #Falar d Try except
 '''
     try:
@@ -10,10 +9,12 @@ from bs4 import BeautifulSoup
     except:
         print("-------!!!!erro na acao:!!!!-------", acao)
 '''
-
 '''
 O código anterior não pegava quando a string vinha como 1.456,76 dessa forma eu troquei tudo que é ponto em espaço vazio e tudo que é virgula em ponto dai fiz transformar em float:
 Linha 50:
 valor = valor.replace(".","").replace(",",".")
 '''
+#MOSTAR O GIT TAMBEM
+#leitura de um arquivo excel para um panda
+#trabalhar com as tabelas do panda
 
