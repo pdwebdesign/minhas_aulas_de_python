b73437109a97cd4b80f1407f1a459702244d4265
Pedro Duarte <pedrohlduarte@gmail.com>
2018-11-08 14:44:40 -0200

    ia iris

---
+++ IA/iris.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+
+#IMPORT DATASET
+iris = load_iris()
+#print(iris.feature_names)
+#print(iris.target_names)
+#print(iris.data[0]) #parametros dessa flor
+#print(iris.target[0]) #a varavel target contem os marcadores
+
+#for i in range(len(iris.target)):
+    #print("Exemplo %d: label %s, features %s" % (i,iris.tagert[i],iris.data[i]))
+ #   print("Dataset Order:",i+1,"Species:",iris.target[i],iris.data[i])
+
+#TRAIN A CLASSIFIER
+
+#O teste é uma parte muito importante no aprendizado de máquinas
